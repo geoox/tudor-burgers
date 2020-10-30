8d65440f30edecd8b6296ab0c1370f4b16b045fb
geoox <georgedobrin11@gmail.com>
2020-10-30 20:13:16 +0100

    added tm menu

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -5,12 +5,15 @@ import SEO from "../components/seo"
 import './menu.css'
 import { Document, Page } from 'react-pdf';
 import menu_pdf from '../images/Menu_Tudor.pdf';
+import menu_tm_pdf from '../images/Menu_Tm.pdf';
 
 export default class MenuPage extends Component{
 
   state = {
-    file: menu_pdf,
-    page: [1,2]
+    file_buc: menu_pdf,
+    page_buc: [1,2],
+    file_tm: menu_tm_pdf,
+    page_tm: [1,2]
   }
 
   render(){
@@ -26,14 +29,29 @@ export default class MenuPage extends Component{
           <div className="menu-page-container">
 
               <Segment placeholder>
-              <br/>
-              <a href={menu_pdf} target="_blank"><b>Click</b> to open the menu in pdf
-              <br/><br/><br/>
+                <a href={menu_tm_pdf} target="_blank"><b>Timisoara</b> menu.
+                <br/><br/><br/>
                 <Document
-                  file={this.state.file}
+                  file={this.state.file_tm}
                   className="view-pc"
                 >
-                  {this.state.page.map(page => (
+                  {this.state.page_tm.map(page => (
+                    <Page pageNumber={page} scale={2.0}/>
+                  ))}
+                
+                </Document>
+                </a>
+              </Segment>
+
+              <Segment placeholder>
+              <br/>
+              <a href={menu_pdf} target="_blank"><b>Bucharest</b> menu.
+              <br/><br/><br/>
+                <Document
+                  file={this.state.file_buc}
+                  className="view-pc"
+                >
+                  {this.state.page_buc.map(page => (
                     <Page pageNumber={page} scale={2.0}/>
                   ))}
                 
